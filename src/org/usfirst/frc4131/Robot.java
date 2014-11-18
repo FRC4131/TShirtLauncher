609c6d9e8151cab13ca5509e45bc3495b96edc8d
Yana Lozina <yaanaa55@gmail.com>
2014-11-18 15:52:21 -0800

    basic driving

--- src/org/usfirst/frc4131/Robot.java
+++ src/org/usfirst/frc4131/Robot.java
@@ -1,45 +1,7 @@
-/*----------------------------------------------------------------------------*/
-<<<<<<< HEAD
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-=======
-/* Copyright (c) FIRST 2008. All Rights Reserved.							 */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.															   */
->>>>>>> 2b94eb8f98de4d9ef3d079eff9c02881cc5043f4
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc4131;
-
 
 import edu.wpi.first.wpilibj.SimpleRobot;
 
-<<<<<<< HEAD
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SimpleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
-public class Robot extends SimpleRobot {
-    /**
-     * This function is called once each time the robot enters autonomous mode.
-     */
-    public void autonomous() {
-        
-    }
-
-    /**
-     * This function is called once each time the robot enters operator control.
-     */
-    public void operatorControl() {
-
-    }
-=======
 public class Robot extends SimpleRobot{
 	public void autonomous(){
 		
@@ -47,5 +9,4 @@ public class Robot extends SimpleRobot{
 	public void operatorControl(){
 		
 	}
->>>>>>> 2b94eb8f98de4d9ef3d079eff9c02881cc5043f4
 }
